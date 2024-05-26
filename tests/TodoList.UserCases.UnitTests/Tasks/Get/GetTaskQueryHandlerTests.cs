d55ecdec5ca45bf00166209382187c53066d9dff
Cesar Mendes da silva <cesar.mendes.silva@outlook.com>
2024-05-26 16:49:36 -0300

    adicionado testes

---
+++ tests/TodoList.UserCases.UnitTests/Tasks/Get/GetTaskQueryHandlerTests.cs
@@ -0,0 +1,40 @@
+﻿using Bogus;
+using MassTransit;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoList.Core.Status.Repositories;
+using TodoList.Core.Tasks.Repositories;
+using TodoList.UserCases.Tasks.Get;
+using TodoList.UserCases.Tasks.Update;
+
+namespace TodoList.UserCases.UnitTests.Tasks.Get
+{
+    public class GetTaskQueryHandlerTests
+    {
+        [Fact]
+        public async Task WhenTaskIsFound_ThenGetTask_ReturnTask()
+        {
+            //Arrange
+            var fake = new Faker<Core.Tasks.Aggregates.Task>();
+
+            var mockTaskRepository = new Mock<ITaskRepository>();
+            var mockLogger = new Mock<ILogger<GetTaskQueryHandler>>();
+
+            mockTaskRepository.Setup(repo => repo.GetAsync(It.IsAny<int>(), It.IsAny<CancellationToken>()))
+                              .ReturnsAsync(fake.Generate());
+
+            var handler = new GetTaskQueryHandler(mockTaskRepository.Object, mockLogger.Object);
+
+            // Act
+            var result = await handler.Handle(new GetTaskQuery(), CancellationToken.None);
+
+            // Assert
+            Assert.NotNull(result);
+        }
+    }
+}

0b4e161f0e7cb2a2f6a4396180af6e8cdd1419ad
Cesar Mendes da silva <cesar.mendes.silva@outlook.com>
2024-05-25 22:50:19 -0300

    ajuste docker file

--- src/todolist.web/vite.config.ts
+++ src/todolist.web/vite.config.ts
@@ -5,12 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    port: 8080
+    port: 3000
   },
   server: {
-    port: 8080,
+      port: 3000,
     strictPort: true,
     host: true,
-    origin: "http://0.0.0.0:8080",
+      origin: "http://0.0.0.0:3000",
    },
 })
